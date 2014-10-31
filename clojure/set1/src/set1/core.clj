d97a22e0068cf22da10794296f9d81ea47773894
Joshua Bangel <falornan@gmail.com>
2014-10-30 22:58:09 -0500

    Clojure 1-1

--- clojure/set1/src/set1/core.clj
+++ clojure/set1/src/set1/core.clj
@@ -1,30 +1,55 @@
 (ns set1.core)
 
-;; (require [clojure.string :as str])
+(require '[clojure.string :as str])
 
 (defn hex-char-value
   "Returns the value for the hex character passed in"
   [hexchar]
-  (.indexOf (seq "0123456789ABCDEF") hexchar))
+  (.indexOf (seq "0123456789ABCDEF") (Character/toUpperCase hexchar)))
+
+(defn hex-pair-to-val
+  "Convert both chars in a hex pair to their Base10 value"
+  ([l r]
+   (+ (bit-shift-left (hex-char-value l) 4) (hex-char-value r)))
+  ([[l r]]
+   (hex-pair-to-val l r)))
+
+(defn hex-string-to-pairs
+  "Split a string of hex values into pairs"
+  [hexstring]
+  (partition 2 (seq (char-array hexstring))))
 
 (defn hex-string-to-vals
   "Convert all characters in the hex string to their corresponding values"
   [hexstring]
-  (map hex-char-value (seq (clojure.string/upper-case hexstring))))
+  (map hex-pair-to-val (hex-string-to-pairs hexstring)))
 
-(defn rhex
-  [s]
-  (map #(apply println %) (partition 2 s)))
+(defn b64val-to-char
+  "Returns the character for an index value"
+  [b64val]
+  (nth (seq "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/") b64val))
+
+(defn trip-to-b64vec
+  ([[a b c]]
+   (vector
+     (b64val-to-char (bit-shift-right a 2))
+     (b64val-to-char (+ (bit-shift-left (bit-and a 2r00000011) 4) (bit-shift-right b 4)))
+     (b64val-to-char (+ (bit-shift-left (bit-and b 2r00001111) 2) (bit-shift-right c 6)))
+     (b64val-to-char (bit-and c 2r00111111))
+    )))
+
+(defn vals-to-b64
+  [vecvals]
+  (str/join (flatten (map trip-to-b64vec (partition 3 vecvals)))))
 
 (defn challenge1
   "Crypto Challenge 1-1"
   [hex-inp]
-  (rhex hex-inp))
+  (vals-to-b64 (hex-string-to-vals hex-inp)))
 
 (defn -main
   [& args]
-  ;; (printf "** %s **\n" (challenge1 "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"))
-  (println (hex-string-to-vals "Abc")))
+  (printf "** %s **\n" (challenge1 "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"))
+  ;;(println (hex-string-to-vals "Abc"))
+  )
 
-(challenge1 "ABCD")
-(println (Character/digit \E 16))
--- clojure/set1/test/set1/core_test.clj
+++ clojure/set1/test/set1/core_test.clj
@@ -2,6 +2,42 @@
   (:require [clojure.test :refer :all]
             [set1.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-char-value-low
+  (testing "hex-char-value [low]"
+    (is (= (hex-char-value \4) 4))))
+
+(deftest test-char-value-high
+  (testing "hex-char-value [high]"
+    (is (= (hex-char-value \D) 13))))
+
+(deftest test-hex-pair-to-val
+  (testing "hex-pair-to-val"
+    (is (= (hex-pair-to-val \3 \D) 61))))
+
+(deftest test-hex-string-to-pairs
+  (testing "hex-string-to-pairs"
+    (is (= (hex-string-to-pairs "ABCD") [[\A \B][\C \D]]))))
+
+(deftest test-hex-string-to-vals
+  (testing 'hex-string-to-vals
+    (is (= (hex-string-to-vals "ABCD1234") [171 205 18 52]))))
+
+(deftest test-trip-to-b64vec
+  (testing "trip-to-b64vec"
+    (is (= (trip-to-b64vec [171 205 18]) [\q \8 \0 \S]))))
+
+(deftest test-b64val-to-char-mid
+  (testing "b64val-to-char [mid]"
+    (is (= (b64val-to-char 42) \q))))
+
+(deftest test-b64val-to-char-high
+  (testing "b64val-to-char [high]"
+    (is (= (b64val-to-char 60) \8))))
+
+(deftest test-b64val-to-char-low
+  (testing "b64val-to-char [low]"
+    (is (= (b64val-to-char 18) \S))))
+
+(deftest test-vals-to-b64
+  (testing "vals-to-b64"
+    (is (= (vals-to-b64 [171 205 18 171 205 18]) "q80Sq80S"))))
